026652e3ac49652cefe9e6fe4e5fef2519e9f0d4
Gajanan Gadekar <gadekar.gajanan15@gmail.com>
2021-04-20 12:52:42 +0530

    Create Account.java
    Account Details like name, accno, etc set in the class

---
+++ Account.java
@@ -0,0 +1,61 @@
+package com.braindata.bankmanage.client;
+
+import java.util.Scanner;
+
+public class Account 
+{
+	private String accNo;
+	private String name;
+	private String mobNo;
+	private String Adharno;
+	private String gender;
+	private String age;
+	private String minBal;
+	
+	
+	public String getMinBal() {
+		return minBal;
+	}
+	public void setMinBal(String minBal) {
+		this.minBal = minBal;
+	}
+	public String getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobNo() {
+		return mobNo;
+	}
+	public void setMobNo(String mobNo) {
+		this.mobNo = mobNo;
+	}
+	public String getAdharno() {
+		return Adharno;
+	}
+	public void setAdharno(String adharno) {
+		Adharno = adharno;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	
+//=====================================================================================================//
+}
