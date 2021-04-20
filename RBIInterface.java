e839d4fa56f8b58bb07290393137447dad3dd3dd
Gajanan Gadekar <gadekar.gajanan15@gmail.com>
2021-04-20 12:55:03 +0530

    Create RBIInterface.java
    RBI interface class in which all method is interface and then implemented

---
+++ RBIInterface.java
@@ -0,0 +1,10 @@
+package com.braindata.bankmanage.model;
+
+public interface RBI 
+{
+	void createAcc();
+	void displayAllaccDetails();
+	void depositMoney();
+	void withdrawal();
+	void accountBalance();
+}
